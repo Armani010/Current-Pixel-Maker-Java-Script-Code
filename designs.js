a032e10b68cf462c4351b5b6cf2bfb92a6e7ad33
Armani010 <aaalexander17@yahoo.com>
2021-12-19 15:18:24 -0600

    Add files via upload

---
+++ designs.js
@@ -0,0 +1,27 @@
+
+let table = document.body.querySelector("#pixelCanvas")
+const myForm = document.body.querySelector("#sizePicker")
+
+myForm.addEventListener("submit", function (event) {
+	event.preventDefault();
+	makeGrid();
+});
+
+function makeGrid() {
+	table.innerHTML=""
+	let currentHeight = document.body.querySelector("#inputHeight").value
+	let currentWidth = document.body.querySelector("#inputWidth").value
+	for (row = 0; row < currentHeight; row++) {
+		var newRow = table.insertRow()
+		for (column = 0; column < currentWidth; column++) {
+			newRow.insertCell()
+		}
+	}
+}
+
+table.addEventListener('click', function(evt){
+	let currentColor = document.body.querySelector("#colorPicker").value
+	if (evt.target.nodeName === 'TD') {
+		td.style.backgroundColor = currentColor;
+	}
+})
