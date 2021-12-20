aa4d19e773f0055d142f1f95bac5bb35fca855a3
Armani010 <aaalexander17@yahoo.com>
2021-12-19 18:31:39 -0600

    Update designs.js

--- designs.js
+++ designs.js
@@ -1,28 +1,28 @@
 const table = document.body.querySelector("#pixelCanvas")
 const myForm = document.body.querySelector("#sizePicker")
 
-myForm.addEventListener("submit", function (event) {
-	event.preventDefault();
-	makeGrid();
-});
-
 function makeGrid() {
 	table.innerHTML=""
 	let currentHeight = document.body.querySelector("#inputHeight").value
 	let currentWidth = document.body.querySelector("#inputWidth").value
 	for (row = 0; row < currentHeight; row++) {
-		var newRow = table.insertRow()
+		let newRow = table.insertRow()
 		for (column = 0; column < currentWidth; column++) {
 			newRow.insertCell()
 		}
 	}
-}
+};
+
+myForm.addEventListener("submit", function (event) {
+	event.preventDefault();
+	makeGrid();
+});
 
 table.addEventListener('click', function(evt) {
 	let currentColor = document.body.querySelector("#colorPicker").value
 	if (evt.target.nodeName === 'TD') {
 		if (evt.target.style.backgroundColor = currentColor) {
-			evt.target.style.backgroundColor = white;
+			evt.target.style.backgroundColor = null;
 		}
 		else {
 			evt.target.style.backgroundColor = currentColor;
